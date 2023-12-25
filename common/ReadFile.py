03cb9252c98caa75aee8f4a8e87727ba6ca181c6
郑秋杰 <15881086121@163.com>
2023-12-25 16:57:39 +0800

    修改代码

--- common/ReadFile.py
+++ common/ReadFile.py
@@ -3,5 +3,7 @@ import configparser
 
 def read_ini(file_path='/socializeTest/config.ini'):
     config = configparser.ConfigParser()
-    config.read(file_path, encoding='utf-8')
+    config.read(file_path, encoding='utf-8-sig')
     return config
+
+
