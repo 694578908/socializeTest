e664fab53853da084d89c689e1efe596c016a0a1
郑秋杰 <15881086121@163.com>
2023-12-25 17:50:00 +0800

    修改代码

--- common/ReadFile.py
+++ common/ReadFile.py
@@ -2,10 +2,8 @@ import configparser
 import os
 
 
-def read_ini(file_path='/socializeTest/config.ini'):
+def read_ini(file_path='/app/config.ini'):
     abs_path = os.path.abspath(file_path)
-    print(f"Absolute path: {abs_path}")
-
     config = configparser.ConfigParser()
     config.read(abs_path, encoding='utf-8-sig')
     return config
