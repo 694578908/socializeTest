b05c775e72d0db953531583d5a2e451b2a1fa20c
郑秋杰 <15881086121@163.com>
2023-12-25 17:38:51 +0800

    Merge branch 'master' of E:\socializeTest with conflicts.

--- common/ReadFile.py
+++ common/ReadFile.py
@@ -1,9 +1,13 @@
 import configparser
+import os
 
 
 def read_ini(file_path='/socializeTest/config.ini'):
+    abs_path = os.path.abspath(file_path)
+    print(f"Absolute path: {abs_path}")
+
     config = configparser.ConfigParser()
-    config.read(file_path, encoding='utf-8-sig')
+    config.read(abs_path, encoding='utf-8-sig')
     return config
 
 
