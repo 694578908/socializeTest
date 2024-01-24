8cc93a05a0d264097a56cf54feba24d637fb97f1
zheng <694578908@qq.com>
2024-01-24 22:36:09 +0800

    修改代码

--- common/request_util.py
+++ common/request_util.py
@@ -17,17 +17,19 @@ class RequestUtil:
         timeout_value = 5
         start_time = time.time()
         if method == 'get':
-            rep = RequestUtil.session.request(method=method, url=url, params=data, headers=headers, timeout=timeout_value,
+            rep = RequestUtil.session.request(method=method, url=url, params=data, headers=headers,
+                                              timeout=timeout_value,
                                               **kwargs)
         else:
             data = json.dumps(data)
-            rep = RequestUtil.session.request(method=method, data=data, url=url, headers=headers, timeout=timeout_value, **kwargs)
+            rep = RequestUtil.session.request(method=method, data=data, url=url, headers=headers, timeout=timeout_value,
+                                              **kwargs)
 
         end_time = time.time()
         elapsed_time = end_time - start_time
         elapsed_time_rounded = round(elapsed_time, 2)
 
-        response_timeout = {'接口预期请求时间': timeout_value, '接口实际请求时间': elapsed_time_rounded}
+        response_timeout = {'接口限制最大请求时间': timeout_value, '接口实际请求时间': elapsed_time_rounded}
         response_url = {'method': rep.request.method, 'url': rep.url}
         allure.attach(json.dumps(response_timeout, ensure_ascii=False, indent=2), name="接口响应时间",
                       attachment_type=allure.attachment_type.JSON)
@@ -44,7 +46,8 @@ class RequestUtil:
             log_status = '接口请求失败'
 
         log_util.log_info(
-            '{}>>>>接口状态码:{},预期请求时间:{}秒,实际请求时间:{}秒'.format(log_status, rep.status_code, timeout_value, elapsed_time_rounded))
+            '{}>>>>接口状态码:{},接口限制最大请求时间:{}秒,实际请求时间:{}秒'.format(log_status, rep.status_code, timeout_value,
+                                                              elapsed_time_rounded))
         log_util.log_info('用例标题:{}'.format(title))
         log_util.log_info('请求地址:{},请求头:{}'.format(rep.url, rep.headers))
         log_util.log_info('请求参数:{}'.format(rep.request.body))
